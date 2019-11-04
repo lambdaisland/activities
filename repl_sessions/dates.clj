21e65e2c233cf092211433259a00848fae37d830
Arne Brasseur <arne@arnebrasseur.net>
2019-11-04 20:15:01 +0000

    Updates notes and adds repl_sessions on dates

---
+++ repl_sessions/dates.clj
@@ -0,0 +1,16 @@
+(ns repl-sessions.dates)
+
+#inst "2019-10-10"
+;; => #inst "2019-10-10T00:00:00.000-00:00"
+
+(type #inst "2019-10-10")
+;; => java.util.Date
+
+;; Yoda time
+
+;; java.time
+
+java.time.Instant
+
+#_clj-time
+#_cloure/java.time ;; https://github.com/dm3/clojure.java-time
