d548801606210f3ec3e5b8a80dc0f579754144eb
Felipe Barros <barrosfelipe@users.noreply.github.com>
2019-11-11 11:03:15 -0300

    Adds support for date-time, duration and capacity

--- src/activities/handlers.clj
+++ src/activities/handlers.clj
@@ -3,7 +3,8 @@
             [clojure.pprint]
             [reitit.core]
             [crux.api :as crux]
-            [activities.flexmark :as flexmark])
+            [activities.flexmark :as flexmark]
+            [java-time :as time])
   (:import [java.util UUID]))
 
 #_
@@ -37,6 +38,7 @@
   [:html
    [:head
     [:title title]
+    [:meta {:name "viewport" :content "width=device-width"}]
     [:link {:rel "stylesheet" :href "/styles.css"}]]
    [:body body]])
 
@@ -62,25 +64,57 @@
 (defn new-activity-form [_]
   (response [:div
              [:form {:method "POST" :action "/activities"}
+              [:header
+               [:h2 "New Activity Proposal"]
+               [:p "So you have an awesome idea for an activity? Publish it to
+                    the community so that others can join you :3"]]
               [:div
                [:label {:for "title"} "Title: "]
-               [:input {:id "title" :name "title" :type "text"}]]
+               [:div
+                [:input {:id "title" :name "title" :type "text"}]]]
               [:div
                [:label {:for "desc"} "Description: "]
-               [:textarea {:id "desc" :name "description" :type "msg"}]]
+               [:div
+                [:textarea {:id "desc" :name "description" :type "msg"}]]]
               [:div
-               [:input {:type "submit"}]]]]))
+               [:label {:for "datetime"} "Date-time: "]
+               [:div
+                [:input {:id "datetime"
+                         :type "datetime-local"
+                         :name "datetime"}]]]
+              [:div
+               [:label {:for "duration"} "And a duration (in minutes): "]
+               [:div
+                [:input {:id "duration"
+                         :type "number"
+                         :name "duration"}]]]
+              [:div
+               [:label {:for "capacity"} "Max. number of participants: "]
+               [:input {:id "capacity"
+                        :type "number"
+                        :name "capacity"
+                        :min 1 :max 10}]]
+              [:div
+               [:div
+                [:input {:type "submit" :value "Submit Activity"}]]]]]))
 
 ;; POST /activity
-(defn create-activity [{{:strs [title description]} :form-params
-                        crux :crux
-                        :as req}]
-  (let [uuid (UUID/randomUUID)]
+(defn create-activity
+  [{{:strs [title description datetime duration capacity]} :form-params
+    crux :crux
+    :as req}]
+  (let [uuid (UUID/randomUUID)
+        date-time (time/local-date-time datetime)
+        duration (time/duration (time/minutes (Integer. duration)))
+        capacity (Integer. capacity)]
     ;; store activity in the database and assign it an id
     (crux/submit-tx crux [[:crux.tx/put
                            {:crux.db/id uuid
                             :activity/title title
-                            :activity/description description}]])
+                            :activity/description description
+                            :activity/date-time date-time
+                            :activity/duration duration
+                            :activity/capacity capacity}]])
     ;; redirect to /activity/<id>
     {:status 303
      :headers {"Location" (path req :activities.system/activity {:id (str uuid)})}}))
@@ -91,11 +125,21 @@
         db          (crux/db (:crux req))
         activity    (crux/entity db id)
         title       (:activity/title activity)
-        description (:activity/description activity)]
+        description (:activity/description activity)
+        date        (time/format "dd/MM/yyyy" (:activity/date-time activity))
+        time        (time/format "hh:mm" (:activity/date-time activity))
+        duration    (time/as (:activity/duration activity) :minutes)
+        capacity    (:activity/capacity activity)]
     (response [:div
                [:h1 title]
                [:div
                 (hiccup/raw (flexmark/md->html description))]
+               [:div
+                [:p (str "On: " date)]
+                [:p (str "At: " time)]
+                [:p (str "For: " duration "m")]]
+               [:div
+                [:p (str "Capacity: " capacity)]]
                [:a {:href (path req :activities.system/edit-activity {:id id})}
                 [:button "EDIT"]]
                [:form {:method "POST" :action (path req :activities.system/activity {:id id})}
@@ -125,11 +169,17 @@
         uuid            (UUID/fromString id)
         new-title       (get-in req [:params :title])
         new-description (get-in req [:params :description])
+        new-date        (time/local-date-time (get-in req [:params :datetime]))
+        new-duration    (time/duration (time/minutes (.Integer (get-in req [:params :duration]))))
+        new-capacity    (.Integer (get-in req [:params :capacity]))
         db              (:crux req)]
     (crux/submit-tx db [[:crux.tx/put
                          {:crux.db/id           uuid
                           :activity/title       new-title
-                          :activity/description new-description}]])
+                          :activity/description new-description
+                          :activity/date-time   new-date
+                          :activity/duration    new-duration
+                          :activity/capacity    new-capacity}]])
     {:status  303
      :headers {"Location" (path req :activities.system/activity {:id id})}}))
 
@@ -141,7 +191,10 @@
         db          (crux/db (:crux req))
         activity    (crux/entity db id)
         title       (:activity/title activity)
-        description (:activity/description activity)]
+        description (:activity/description activity)
+        datetime    (str (time/truncate-to (:activity/date-time activity) :minutes))
+        duration    (time/as (:activity/duration activity) :minutes)
+        capacity    (:activity/capacity activity)]
     (response [:div
                [:form
                 {:method "POST"
@@ -154,6 +207,15 @@
                  [:textarea {:id "description" :name "description" :type "msg"}
                   :value description]]
                 [:div
+                 [:label {:for "datetime"} "Date-time: "]
+                 [:input {:id "datetime" :type "datetime-local" :name "datetime" :value datetime}]]
+                [:div
+                 [:label {:for "duration"} "Duration: "]
+                 [:input {:id "duration" :type "number" :name "duration" :value (str duration)}]]
+                [:div
+                 [:label {:for "capacity"} "Capacity: "]
+                 [:input {:id "capacity" :type "number" :name "capacity" :value (str capacity)}]]
+                [:div
                  [:input {:type "submit"}]]]])))
 
 ;; DELETE /activity/:id
@@ -164,3 +226,10 @@
     (crux/submit-tx db [[:crux.tx/delete uuid]])
     (response [:div
                [:p "Activity successfully deleted."]])))
+
+(declare login-form login-submission register-form register-submission)
+
+;; TODO: GET /login
+;; TODO: POST /login
+;; TODO: GET /register
+;; TODO: POST /register
