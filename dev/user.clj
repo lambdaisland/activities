7addceb7341de2a0846e859a13c14b692c7c08a5
Felipe Barros <barrosfelipe@users.noreply.github.com>
2019-11-11 11:00:33 -0300

    Helper functions to get a list of ids and to clear the db

--- dev/user.clj
+++ dev/user.clj
@@ -17,3 +17,13 @@
    (crux/db
     (:crux integrant.repl.state/system))
    query))
+
+(defn get-uuids []
+  (q '{:find [uuid]
+       :where [[uuid :crux.db/id]]}))
+
+(defn mount-delete-ops []
+  (into [] (map (fn [[uuid]] [:crux.tx/delete uuid]) (get-uuids))))
+
+(defn clear-db []
+  (crux/submit-tx (crux) (mount-delete-ops)))
