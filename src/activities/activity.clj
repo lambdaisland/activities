51ba2ee4b9a5b699ceda701943738107a8f5d6c0
Arne Brasseur <arne@arnebrasseur.net>
2019-11-26 17:30:37 +0000

    New activity and user namespaces for organization

---
+++ src/activities/activity.clj
@@ -0,0 +1,28 @@
+(ns activities.activity
+  (:require [clojure.spec.alpha :as s]
+            [java-time :as time]
+            ))
+
+
+(s/def ::title string?)
+(s/def ::description string?)
+(s/def ::date-time inst?)
+(s/def ::duration int?)
+(s/def ::capacity int?)
+;; (s/def ::creator uuid?)
+(s/def ::participants
+  (s/coll-of uuid? :kind set? :distinct true :min-count 0 :into #{}))
+
+(s/def :activities/activity
+  (s/keys :req [:crux.db/id ::title ::date-time ::duration ::capacity]
+          :opts [::description ::participants]))
+
+(defn params->activity [{:strs [title description datetime duration capacity]}]
+  (let [date-time (time/local-date-time datetime) ;; TODO use inst
+        duration  (Long/parseLong duration)
+        capacity  (Long/parseLong capacity)]
+    {::title       title
+     ::description description
+     ::date-time   date-time
+     ::duration    duration
+     ::capacity    capacity}))
