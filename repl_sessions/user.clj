515d4afd06e8a469f3c2410002527fe3e2fcd18e
Arne Brasseur <arne@arnebrasseur.net>
2019-11-26 17:30:02 +0000

    Facilities to check user creation

---
+++ repl_sessions/user.clj
@@ -0,0 +1,19 @@
+(ns repl-sessions.user)
+
+(last @user/last-requests)
+
+(nth (reverse @user/last-requests) 3)
+
+(def create-activity
+  (last
+   (filter (comp #{:post} :request-method) @user/last-requests)))
+
+
+create-activity
+
+(activities.user/req->id (last @user/last-requests))
+
+(user/entity #uuid "4415f920-5f5b-4277-87e7-da0cb9ac221d")
+(user/entity #uuid "abf230e5-2af2-4c44-8490-1864857d0b69")
+
+(user/q )
