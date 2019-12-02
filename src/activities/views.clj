fc6af52d7935f981825d3fa115c2803752476799
Felipe Barros <barrosfelipe@users.noreply.github.com>
2019-12-02 14:46:39 -0300

    Move activity-form markup to views namespace

--- src/activities/views.clj
+++ src/activities/views.clj
@@ -133,3 +133,39 @@
 ;;   [:main
 ;;    [:header [:h1 "List of activities"]]
 ;;    [:div activities]])
+
+(defn activity-form []
+  [:div
+   [:form {:method "POST" :action "/activities"}
+    [:header
+     [:h2 "New Activity Proposal"]]
+    [:div
+     [:label {:for "title"} "Title: "]
+     [:div
+      [:input {:id "title" :name "title" :type "text"}]]]
+    [:div
+     [:label {:for "desc"} "Description: "]
+     [:div
+      [:textarea {:id "desc" :name "description" :type "msg"}]]]
+    [:div
+     [:label {:for "datetime"} "Date-time: "]
+     [:div
+      [:input {:id "datetime"
+               :type "datetime-local"
+               :name "datetime"}]]]
+    [:div
+     [:label {:for "duration"} "And a duration (in minutes): "]
+     [:div
+      [:input {:id "duration"
+               :type "number"
+               :name "duration"}]]]
+    [:div
+     [:label {:for "capacity"} "Max. number of participants: "]
+     [:input {:id "capacity"
+              :type "number"
+              :name "capacity"
+              :min 1 :max 10}]]
+    [:div
+     [:div
+      [:input {:type "submit" :value "Submit Activity"}]]]]])
+
